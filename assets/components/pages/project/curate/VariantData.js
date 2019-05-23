18c884ae3bad08b7b2f01fa7bafc86211d3aa9d9
Nick Watts <nwatts@broadinstitute.org>
2019-05-23 13:30:04 -0400

    Display LOFTEE output on curate page

--- assets/components/pages/project/curate/VariantData.js
+++ assets/components/pages/project/curate/VariantData.js
@@ -2,6 +2,65 @@ import PropTypes from "prop-types";
 import React from "react";
 import { List } from "semantic-ui-react";
 
+const AnnotationsList = ({ annotations }) => {
+  const annotationsGroupedByGene = annotations.reduce(
+    (acc, annotation) => ({
+      ...acc,
+      [annotation.gene_id]: [...(acc[annotation.gene_id] || []), annotation],
+    }),
+    {}
+  );
+
+  return (
+    <List>
+      {Object.keys(annotationsGroupedByGene).map(geneId => (
+        <List.Item key={geneId}>
+          <List.Header>{annotationsGroupedByGene[geneId][0].gene_symbol}</List.Header>
+          <List.List>
+            {annotationsGroupedByGene[geneId].map(annotation => (
+              <List.Item key={annotation.transcript_id}>
+                <List.Content>
+                  <List.Header>{annotation.transcript_id}</List.Header>
+                  <List.Description>
+                    {annotation.consequence}
+                    {annotation.loftee && (
+                      <React.Fragment>
+                        <br />
+                        LOFTEE: {annotation.loftee}
+                        {annotation.loftee === "LC" && ` (${annotation.loftee_filter})`}
+                      </React.Fragment>
+                    )}
+                    {annotation.loftee_flags && (
+                      <React.Fragment>
+                        <br />
+                        LOFTEE Flags: {annotation.loftee_flags}
+                      </React.Fragment>
+                    )}
+                  </List.Description>
+                </List.Content>
+              </List.Item>
+            ))}
+          </List.List>
+        </List.Item>
+      ))}
+    </List>
+  );
+};
+
+AnnotationsList.propTypes = {
+  annotations: PropTypes.arrayOf(
+    PropTypes.shape({
+      consequence: PropTypes.string,
+      gene_id: PropTypes.string,
+      gene_symbol: PropTypes.string,
+      transcript_id: PropTypes.string,
+      loftee: PropTypes.string,
+      loftee_filter: PropTypes.string,
+      loftee_flags: PropTypes.string,
+    })
+  ).isRequired,
+};
+
 const VariantData = ({ variant }) => {
   return (
     <React.Fragment>
@@ -20,13 +79,7 @@ const VariantData = ({ variant }) => {
         </List.Item>
         <List.Item>
           <strong>Annotations:</strong>
-          <List>
-            {variant.annotations.map(annotation => (
-              <List.Item key={annotation.transcript_id}>
-                {annotation.transcript_id}: {annotation.consequence}
-              </List.Item>
-            ))}
-          </List>
+          <AnnotationsList annotations={variant.annotations} />
         </List.Item>
       </List>
     </React.Fragment>
@@ -39,14 +92,7 @@ VariantData.propTypes = {
     AC: PropTypes.number,
     AN: PropTypes.number,
     AF: PropTypes.number,
-    annotations: PropTypes.arrayOf(
-      PropTypes.shape({
-        consequence: PropTypes.string,
-        gene_id: PropTypes.string,
-        gene_symbol: PropTypes.string,
-        transcript_id: PropTypes.string,
-      })
-    ).isRequired,
+    annotations: PropTypes.arrayOf(PropTypes.object).isRequired,
   }).isRequired,
 };
 
