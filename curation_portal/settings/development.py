78a32ab758200268ec2b18286a866bda754ccb54
Nick Watts <nwatts@broadinstitute.org>
2019-05-23 16:35:03 -0400

    Limit debug logs in development settings

--- curation_portal/settings/development.py
+++ curation_portal/settings/development.py
@@ -10,7 +10,9 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "handlers": {"console": {"level": "DEBUG", "class": "logging.StreamHandler"}},
-    "loggers": {"django": {"handlers": ["console"], "level": "DEBUG", "propagate": False}},
+    "loggers": {
+        "django.db.backends": {"handlers": ["console"], "level": "DEBUG", "propagate": False}
+    },
 }
 
 SECURE_SSL_REDIRECT = False
