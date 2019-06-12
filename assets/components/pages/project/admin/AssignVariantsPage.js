64a6e270a605d249e69ae13d2c05ebe7dd35da25
Nick Watts <nwatts@broadinstitute.org>
2019-06-12 11:24:13 -0400

    Add instructions for importing assignments from file

--- assets/components/pages/project/admin/AssignVariantsPage.js
+++ assets/components/pages/project/admin/AssignVariantsPage.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Button, Form, Header, Icon, List, Message } from "semantic-ui-react";
+import { Button, Form, Header, Icon, List, Message, Segment } from "semantic-ui-react";
 
 import { PermissionRequired } from "../../../../permissions";
 import getCookie from "../../../../utilities/getCookie";
@@ -92,6 +92,11 @@ class AssignVariantsPage extends Component {
         <Header as="h1" dividing>
           {project.name}
         </Header>
+        <div>
+          <Link to={`/project/${project.id}/admin/`}>Return to project</Link>
+        </div>
+        <br />
+
         <PermissionRequired user={user} action="edit" resourceType="project" resource={project}>
           <Form
             ref={this.form}
@@ -99,70 +104,85 @@ class AssignVariantsPage extends Component {
             style={{ marginBottom: "1rem" }}
             onSubmit={this.onSubmit}
           >
-            <List>
-              {assignments.map((assignment, index) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <List.Item key={index}>
-                  {assignment.curator}: {assignment.variant_id}
-                </List.Item>
-              ))}
-            </List>
-            <Form.Group widths={2}>
-              <Form.Input
-                label="Curator"
-                placeholder="Curator"
-                value={assignmentCurator}
-                onChange={(e, { value }) => {
-                  this.setState({ assignmentCurator: value });
+            <Segment attached>
+              <Header as="h4">Add assignment</Header>
+              <Form.Group widths={2}>
+                <Form.Input
+                  label="Curator"
+                  placeholder="Curator"
+                  value={assignmentCurator}
+                  onChange={(e, { value }) => {
+                    this.setState({ assignmentCurator: value });
+                  }}
+                />
+                <Form.Input
+                  label="Variant"
+                  placeholder="Variant"
+                  value={assignmentVariant}
+                  onChange={(e, { value }) => {
+                    this.setState({ assignmentVariant: value });
+                  }}
+                />
+              </Form.Group>
+              <Button
+                disabled={!(assignmentCurator && assignmentVariant)}
+                type="button"
+                onClick={() => {
+                  this.setState(state => ({
+                    assignments: [
+                      ...state.assignments,
+                      { curator: state.assignmentCurator, variant_id: state.assignmentVariant },
+                    ],
+                    assignmentCurator: "",
+                    assignmentVariant: "",
+                  }));
                 }}
-              />
-              <Form.Input
-                label="Variant"
-                placeholder="Variant"
-                value={assignmentVariant}
-                onChange={(e, { value }) => {
-                  this.setState({ assignmentVariant: value });
-                }}
-              />
-            </Form.Group>
-            <Button
-              disabled={!(assignmentCurator && assignmentVariant)}
-              type="button"
-              onClick={() => {
-                this.setState(state => ({
-                  assignments: [
-                    ...state.assignments,
-                    { curator: state.assignmentCurator, variant_id: state.assignmentVariant },
-                  ],
-                  assignmentCurator: "",
-                  assignmentVariant: "",
-                }));
-              }}
-            >
-              Add
-            </Button>
+              >
+                Add
+              </Button>
+            </Segment>
             <br />
+
+            <Segment attached>
+              <Header as="h4">Import assignments from file</Header>
+              <Button as="label" htmlFor="assignments-file">
+                <Icon name="upload" />
+                Choose file
+                <input
+                  hidden
+                  id="assignments-file"
+                  type="file"
+                  onChange={e => this.onSelectFile(e.target.files[0])}
+                />
+              </Button>
+            </Segment>
+            <Message attached size="mini">
+              <Icon name="help" /> This should be a CSV file with two columns: curator&apos;s
+              username and variant ID (chrom-pos-ref-alt). It should not have a header row.
+            </Message>
             <br />
-            <Button as="label" htmlFor="assignments-file">
-              <Icon name="upload" />
-              Import assignments from file
-              <input
-                hidden
-                id="assignments-file"
-                type="file"
-                onChange={e => this.onSelectFile(e.target.files[0])}
-              />
-            </Button>
-            <br />
-            <br />
-            {saveError && <Message error header="Failed to assign curators" />}
-            <Button disabled={assignments.length === 0 || isSaving} primary type="submit">
-              Assign curators
-            </Button>
+
+            {assignments.length > 0 && (
+              <React.Fragment>
+                <Segment attached>
+                  <List>
+                    {assignments.map((assignment, index) => (
+                      // eslint-disable-next-line react/no-array-index-key
+                      <List.Item key={index}>
+                        {assignment.curator}: {assignment.variant_id}
+                      </List.Item>
+                    ))}
+                  </List>
+                </Segment>
+                <Segment attached>
+                  <Button disabled={assignments.length === 0 || isSaving} primary type="submit">
+                    Assign curators
+                  </Button>
+                  {saveError && <Message error header="Failed to assign curators" />}
+                </Segment>
+              </React.Fragment>
+            )}
           </Form>
-          <Button as={Link} to={`/project/${project.id}/admin/`}>
-            Cancel
-          </Button>
         </PermissionRequired>
       </React.Fragment>
     );
