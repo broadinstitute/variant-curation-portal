6f4c2d7f183de8b675fbf388d833ee8df42eeedd
Nick Watts <nwatts@broadinstitute.org>
2019-06-28 10:59:02 -0400

    Run webpack-dev-server with yarn instead of modifying path

--- start.sh
+++ start.sh
@@ -4,13 +4,12 @@ cd $(dirname "${BASH_SOURCE}")
 
 export DJANGO_SETTINGS_MODULE="curation_portal.settings.development"
 
-export PATH=$PATH:./node_modules/.bin
 export NODE_ENV=development
 
 ./manage.py runserver &
 DJANGO_PID=$!
 
-webpack-dev-server --hot &
+yarn run webpack-dev-server --hot &
 WEBPACK_PID=$!
 
 trap "kill ${WEBPACK_PID}; pkill -P ${DJANGO_PID}; exit 1" INT
