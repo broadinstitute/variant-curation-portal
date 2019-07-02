20fffa40dfad0988564565f4e00e87015ae710b6
Nick Watts <nwatts@broadinstitute.org>
2019-07-02 18:22:05 -0400

    Remove unnecessary Babel plugin

--- babel.config.js
+++ babel.config.js
@@ -12,5 +12,5 @@ module.exports = {
       },
     ],
   ],
-  plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/plugin-proposal-class-properties"],
+  plugins: ["@babel/plugin-proposal-class-properties"],
 };
