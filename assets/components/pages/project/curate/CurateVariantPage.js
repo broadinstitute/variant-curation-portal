f8dedaf7c4ebd68e3644bd9788c2823c3ba83d80
Nick Watts <nwatts@broadinstitute.org>
2019-07-30 14:38:02 -0400

    Rearrange form and controls on curation page

--- assets/components/pages/project/curate/CurateVariantPage.js
+++ assets/components/pages/project/curate/CurateVariantPage.js
@@ -1,13 +1,12 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Header, List, Segment } from "semantic-ui-react";
+import { Button, Divider, Header, List, Segment } from "semantic-ui-react";
 
 import DocumentTitle from "../../../DocumentTitle";
 import Fetch from "../../../Fetch";
 import KeyboardShortcut, { KeyboardShortcutHint } from "../../../KeyboardShortcut";
 import VariantId from "../../../VariantId";
-import Page from "../../Page";
 import withParamsAsProps from "../../withParamsAsProps";
 
 import CurationForm from "./CurationForm";
@@ -61,30 +60,20 @@ class CurateVariantPage extends React.Component {
             const hasAnnotations = variant.annotations.length > 0;
 
             return (
-              <Page>
-                <DocumentTitle title={`${project.name} | ${variant.variant_id}`} />
-                <Header as="h1" dividing id="top">
-                  {project.name} | <VariantId variantId={variant.variant_id} />
-                </Header>
+              <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
                 <div
                   style={{
-                    position: "fixed",
-                    right: "10px",
-                    top: "60px",
-                    zIndex: 1,
+                    padding: "1.5rem 1.5rem 1rem",
+                    border: "1px solid rgba(34, 36, 38, 0.15)",
+                    boxShadow: "0px 1px 2px 0 rgba(34, 36, 38, 0.15)",
                   }}
                 >
-                  <Segment raised>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                      }}
-                    >
-                      <span>
-                        {project.name} | {index + 1}: <VariantId variantId={variant.variant_id} />
-                      </span>
+                  <DocumentTitle title={`${project.name} | ${variant.variant_id}`} />
+                  <div style={{ display: "flex" }}>
+                    <Header as="h1" style={{ flexGrow: 1, marginBottom: "0.21428571rem" }}>
+                      {project.name} | {index + 1}: <VariantId variantId={variant.variant_id} />
+                    </Header>
+                    <div style={{ flexShrink: 0 }}>
                       <List horizontal>
                         <List.Item>
                           {previousVariant ? (
@@ -132,120 +121,114 @@ class CurateVariantPage extends React.Component {
                             "next variant"
                           )}
                         </List.Item>
+                        <List.Item>
+                          <Button
+                            basic
+                            size="small"
+                            onClick={() => {
+                              this.setState(state => ({ ...state, showForm: !state.showForm }));
+                            }}
+                          >
+                            {showForm ? "Hide" : "Show"} form{" "}
+                            <KeyboardShortcutHint color="rgba(0, 0, 0, 0.4)" keys="f" />
+                          </Button>
+                          <KeyboardShortcut
+                            keys="f"
+                            onShortcut={() => {
+                              this.setState(state => ({ ...state, showForm: !state.showForm }));
+                            }}
+                          />
+                        </List.Item>
                       </List>
                     </div>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                      }}
-                    >
-                      <span>
-                        Jump to:
-                        <List horizontal style={{ marginLeft: "0.5rem", marginRight: "3rem" }}>
-                          <List.Item>
-                            <a href="#top">top</a>
-                          </List.Item>
-                          <List.Item>
-                            {gnomadVariantId ? (
-                              <a href="#gnomad-variant">gnomAD (variant)</a>
-                            ) : (
-                              "gnomAD (variant)"
-                            )}
-                          </List.Item>
-                          <List.Item>
-                            {hasAnnotations ? (
-                              <a href="#gnomad-gene">gnomAD (gene)</a>
-                            ) : (
-                              "gnomAD (gene)"
-                            )}
-                          </List.Item>
-                          <List.Item>
-                            <a href="#ucsc">UCSC (variant)</a>
-                          </List.Item>
-                          <List.Item>
-                            {hasAnnotations ? <a href="#ucsc-gene">UCSC (gene)</a> : "UCSC (gene)"}
-                          </List.Item>
-                        </List>
-                      </span>
-                      <Button
-                        basic
-                        size="small"
-                        onClick={() => {
-                          this.setState(state => ({ ...state, showForm: !state.showForm }));
-                        }}
-                      >
-                        {showForm ? "Hide" : "Show"} form{" "}
-                        <KeyboardShortcutHint color="rgba(0, 0, 0, 0.4)" keys="f" />
-                      </Button>
-                      <KeyboardShortcut
-                        keys="f"
-                        onShortcut={() => {
-                          this.setState(state => ({ ...state, showForm: !state.showForm }));
-                        }}
-                      />
-                    </div>
+                  </div>
+                  <Divider style={{ margin: "0 0 1rem" }} />
+                  <div>
+                    <Link to={`/project/${project.id}/`}>All variants in this project</Link>
+                  </div>
+                  <div>
+                    Jump to:
+                    <List horizontal style={{ marginLeft: "0.5rem", marginRight: "3rem" }}>
+                      <List.Item>
+                        <a href="#top">top</a>
+                      </List.Item>
+                      <List.Item>
+                        {gnomadVariantId ? (
+                          <a href="#gnomad-variant">gnomAD (variant)</a>
+                        ) : (
+                          "gnomAD (variant)"
+                        )}
+                      </List.Item>
+                      <List.Item>
+                        {hasAnnotations ? (
+                          <a href="#gnomad-gene">gnomAD (gene)</a>
+                        ) : (
+                          "gnomAD (gene)"
+                        )}
+                      </List.Item>
+                      <List.Item>
+                        <a href="#ucsc">UCSC (variant)</a>
+                      </List.Item>
+                      <List.Item>
+                        {hasAnnotations ? <a href="#ucsc-gene">UCSC (gene)</a> : "UCSC (gene)"}
+                      </List.Item>
+                    </List>
+                  </div>
 
-                    <div
-                      style={
-                        showForm
-                          ? {
-                              maxHeight: "calc(100vh - 200px)",
-                              overflowX: "hidden",
-                              overflowY: "auto",
-                            }
-                          : { display: "none" }
-                      }
-                    >
+                  <div
+                    style={
+                      showForm
+                        ? {
+                            position: "fixed",
+                            right: "1.5rem",
+                            top: "110px",
+                            zIndex: 1,
+                            maxHeight: "calc(100vh - 145px)",
+                            overflowX: "hidden",
+                            overflowY: "auto",
+                          }
+                        : { display: "none" }
+                    }
+                  >
+                    <Segment raised>
                       <CurationForm
                         ref={this.curationForm}
                         projectId={project.id}
                         variantId={variantId}
                         initialResult={result || {}}
                       />
-                    </div>
-                  </Segment>
+                    </Segment>
+                  </div>
                 </div>
 
-                <div>
-                  <Link to={`/project/${project.id}/`}>All variants in this project</Link>
+                <div
+                  style={{
+                    flexGrow: 1,
+                    height: "100%",
+                    overflow: "auto",
+                    padding: "0 1.5rem 1.5rem",
+                  }}
+                >
+                  <a id="top" /> {/* eslint-disable-line */}
+                  <VariantData variant={variant} />
+                  {variant.samples.length ? (
+                    <SampleTable samples={variant.samples} />
+                  ) : (
+                    <Segment placeholder textAlign="center" style={{ minHeight: "6rem" }}>
+                      <Header>Sample data not available for this variant</Header>
+                    </Segment>
+                  )}
+                  <hr style={{ margin: "30px 0" }} />
+                  <GnomadVariantView variant={variant} />
+                  <br />
+                  <GnomadGeneView variant={variant} />
+                  <br />
+                  <UCSCVariantView variant={variant} />
+                  <br />
+                  <UCSCGeneView variant={variant} />
+                  <br />
                 </div>
-
-                {/**
-                Variant information
-               */}
-                <VariantData variant={variant} />
-
-                {/**
-                Sample list
-               */}
-                {variant.samples.length ? (
-                  <SampleTable samples={variant.samples} />
-                ) : (
-                  <Segment placeholder textAlign="center" style={{ minHeight: "6rem" }}>
-                    <Header>Sample data not available for this variant</Header>
-                  </Segment>
-                )}
-
-                <hr style={{ margin: "30px 0" }} />
-
-                {/**
-                References
-               */}
-
-                <GnomadVariantView variant={variant} />
-                <br />
-
-                <GnomadGeneView variant={variant} />
-                <br />
-
-                <UCSCVariantView variant={variant} />
-                <br />
-
-                <UCSCGeneView variant={variant} />
-                <br />
-              </Page>
+              </div>
             );
           }}
         </Fetch>
