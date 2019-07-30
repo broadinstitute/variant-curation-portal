44d934c054d7e19de020f004b87489c036fd8752
Nick Watts <nwatts@broadinstitute.org>
2019-07-30 16:34:31 -0400

    Sort assigned projects by number of incomplete assignments

--- tests/test_projects_list_views.py
+++ tests/test_projects_list_views.py
@@ -2,7 +2,7 @@
 import pytest
 from rest_framework.test import APIClient
 
-from curation_portal.models import CurationAssignment, Project, User
+from curation_portal.models import CurationAssignment, CurationResult, Project, User
 
 pytestmark = pytest.mark.django_db  # pylint: disable=invalid-name
 
@@ -15,6 +15,12 @@ def db_setup(django_db_setup, django_db_blocker, create_variant):
 
         project2 = Project.objects.create(name="Project #2")
         variant2 = create_variant(project2, "2-300-C-G")
+        variant3 = create_variant(project2, "3-200-G-T")
+
+        project3 = Project.objects.create(name="Project #3")
+        variant4 = create_variant(project3, "1-100-T-A")
+        variant5 = create_variant(project3, "1-200-C-A")
+        variant6 = create_variant(project3, "1-300-G-C")
 
         user1 = User.objects.create(username="user1@example.com")
         user2 = User.objects.create(username="user2@example.com")
@@ -24,12 +30,27 @@ def db_setup(django_db_setup, django_db_blocker, create_variant):
         project1.owners.set([user1, user2])
         project2.owners.set([user1])
         CurationAssignment.objects.create(curator=user2, variant=variant1)
+        CurationAssignment.objects.create(curator=user2, variant=variant2)
+        CurationAssignment.objects.create(curator=user2, variant=variant3)
+        CurationAssignment.objects.create(curator=user2, variant=variant4)
+        assignment = CurationAssignment.objects.create(curator=user2, variant=variant5)
+        result = CurationResult(verdict="lof")
+        result.save()
+        assignment.result = result
+        assignment.save()
+        assignment = CurationAssignment.objects.create(curator=user2, variant=variant6)
+        result = CurationResult(verdict="lof")
+        result.save()
+        assignment.result = result
+        assignment.save()
+
         CurationAssignment.objects.create(curator=user3, variant=variant2)
 
         yield
 
         project1.delete()
         project2.delete()
+        project3.delete()
 
         user1.delete()
         user2.delete()
@@ -78,7 +99,7 @@ def test_assigned_projects_list_requires_authentication(db_setup):
     "username,expected_projects",
     [
         ("user1@example.com", []),
-        ("user2@example.com", ["Project #1"]),
+        ("user2@example.com", ["Project #1", "Project #2", "Project #3"]),
         ("user3@example.com", ["Project #2"]),
         ("user4@example.com", []),
     ],
@@ -88,4 +109,13 @@ def test_assigned_projects_list_includes_assigned_projects(db_setup, username, e
     client.force_authenticate(User.objects.get(username=username))
     response = client.get("/api/assignments/").json()
     assigned_projects = [project["name"] for project in response["projects"]]
-    assert assigned_projects == expected_projects
+    assert set(assigned_projects) == set(expected_projects)
+
+
+def test_assigned_projects_list_is_sorted_by_number_of_incomplete_assignments(db_setup):
+    client = APIClient()
+    client.force_authenticate(User.objects.get(username="user2@example.com"))
+    response = client.get("/api/assignments/").json()
+    assigned_projects = [project["name"] for project in response["projects"]]
+    print(assigned_projects)
+    assert assigned_projects == ["Project #2", "Project #3", "Project #1"]
