98dd381cf328b23909ebc487ac23ea0612698a0a
Nick Watts <nwatts@broadinstitute.org>
2019-07-31 11:18:40 -0400

    Remove BasePage component

--- assets/components/pages/projects/AssignedProjectsPage.js
+++ assets/components/pages/projects/AssignedProjectsPage.js
@@ -3,41 +3,44 @@ import { Link } from "react-router-dom";
 import { Header, Item } from "semantic-ui-react";
 
 import DocumentTitle from "../../DocumentTitle";
-import BasePage from "../BasePage";
+import Fetch from "../../Fetch";
 import Page from "../Page";
 
 const AssignedProjectsPage = () => {
   return (
-    <BasePage dataPath="/assignments/">
-      {({ data }) => {
-        return (
-          <Page>
-            <Header as="h1" dividing>
-              Assignments
-            </Header>
-            <DocumentTitle title="Assignments" />
-            {data.projects.length > 0 ? (
-              <Item.Group>
-                {data.projects.map(project => (
-                  <Item key={project.id}>
-                    <Item.Content>
-                      <Item.Header>
-                        <Link to={`/project/${project.id}/`}>{project.name}</Link>
-                      </Item.Header>
-                      <Item.Meta>
-                        {project.variants_curated} / {project.variants_assigned} variants curated
-                      </Item.Meta>
-                    </Item.Content>
-                  </Item>
-                ))}
-              </Item.Group>
-            ) : (
-              <p>No assignments.</p>
-            )}
-          </Page>
-        );
-      }}
-    </BasePage>
+    <Page>
+      <DocumentTitle title="Assignments" />
+      <Header as="h1" dividing>
+        Assignments
+      </Header>
+
+      <Fetch path="/assignments/">
+        {({ data }) => {
+          return (
+            <React.Fragment>
+              {data.projects.length > 0 ? (
+                <Item.Group>
+                  {data.projects.map(project => (
+                    <Item key={project.id}>
+                      <Item.Content>
+                        <Item.Header>
+                          <Link to={`/project/${project.id}/`}>{project.name}</Link>
+                        </Item.Header>
+                        <Item.Meta>
+                          {project.variants_curated} / {project.variants_assigned} variants curated
+                        </Item.Meta>
+                      </Item.Content>
+                    </Item>
+                  ))}
+                </Item.Group>
+              ) : (
+                <p>No assignments.</p>
+              )}
+            </React.Fragment>
+          );
+        }}
+      </Fetch>
+    </Page>
   );
 };
 
--- assets/components/pages/projects/ProjectsPage.js
+++ assets/components/pages/projects/ProjectsPage.js
@@ -5,46 +5,49 @@ import { Button, Header, Item } from "semantic-ui-react";
 
 import { can } from "../../../permissions";
 import DocumentTitle from "../../DocumentTitle";
-import BasePage from "../BasePage";
+import Fetch from "../../Fetch";
 import Page from "../Page";
 
 const ProjectsPage = ({ user }) => {
   return (
-    <BasePage dataPath="/projects/">
-      {({ data }) => {
-        return (
-          <Page>
-            <Header as="h1" dividing>
-              Projects
-            </Header>
-            <DocumentTitle title="Projects" />
-            {data.projects.length > 0 ? (
-              <React.Fragment>
-                <Item.Group>
-                  {data.projects.map(project => (
-                    <Item key={project.id}>
-                      <Item.Content>
-                        <Item.Header>
-                          <Link to={`/project/${project.id}/admin/`}>{project.name}</Link>
-                        </Item.Header>
-                      </Item.Content>
-                    </Item>
-                  ))}
-                </Item.Group>
-              </React.Fragment>
-            ) : (
-              <p>No projects.</p>
-            )}
+    <Page>
+      <DocumentTitle title="Projects" />
+      <Header as="h1" dividing>
+        Projects
+      </Header>
 
-            {can(user, "add", "project") && (
-              <Button as={Link} to="/projects/create">
-                Create project
-              </Button>
-            )}
-          </Page>
-        );
-      }}
-    </BasePage>
+      <Fetch path="/projects/">
+        {({ data }) => {
+          return (
+            <React.Fragment>
+              {data.projects.length > 0 ? (
+                <React.Fragment>
+                  <Item.Group>
+                    {data.projects.map(project => (
+                      <Item key={project.id}>
+                        <Item.Content>
+                          <Item.Header>
+                            <Link to={`/project/${project.id}/admin/`}>{project.name}</Link>
+                          </Item.Header>
+                        </Item.Content>
+                      </Item>
+                    ))}
+                  </Item.Group>
+                </React.Fragment>
+              ) : (
+                <p>No projects.</p>
+              )}
+
+              {can(user, "add", "project") && (
+                <Button as={Link} to="/projects/create">
+                  Create project
+                </Button>
+              )}
+            </React.Fragment>
+          );
+        }}
+      </Fetch>
+    </Page>
   );
 };
 
