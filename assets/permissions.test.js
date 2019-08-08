6fcdd196c5ff0d50c85d463d3b063ffe04cb7cba
Nick Watts <nwatts@broadinstitute.org>
2019-08-08 16:26:56 -0400

    Add tests for client side permissions checks

---
+++ assets/permissions.test.js
@@ -0,0 +1,61 @@
+import { can } from "./permissions";
+
+describe("can", () => {
+  describe("add project", () => {
+    it("should return false if a user does not have the add_project permission", () => {
+      expect(can({ username: "user1", permissions: [] }, "add", "project")).toBe(false);
+    });
+
+    it("should return true if a user does not have the add_project permission", () => {
+      expect(can({ username: "user1", permissions: ["add_project"] }, "add", "project")).toBe(true);
+    });
+  });
+
+  describe("edit project", () => {
+    // The project API should not include the owners field if the user is not an owner.
+    it("should return false if the project does not have an owners list", () => {
+      expect(
+        can({ username: "user1" }, "edit", "project", {
+          name: "Project Foo",
+        })
+      ).toBe(false);
+    });
+
+    // This should never happen. If the user is not a project owner, then the API
+    // should not return the owners list.
+    it("should return false if the user is not in the owners list", () => {
+      expect(
+        can({ username: "user1" }, "edit", "project", {
+          name: "Project Foo",
+          owners: ["user2"],
+        })
+      ).toBe(false);
+    });
+
+    it("should return true if the user is in the owners list", () => {
+      expect(
+        can({ username: "user1" }, "edit", "project", {
+          name: "Project Foo",
+          owners: ["user0", "user1", "user2"],
+        })
+      ).toBe(true);
+    });
+  });
+
+  describe("unknown actions and resources", () => {
+    it("should throw an error for unknown actions", () => {
+      expect(() => {
+        can({ username: "user1" }, "remove", "project", {
+          name: "Project Foo",
+          owners: ["user1"],
+        });
+      }).toThrow();
+    });
+
+    it("should throw an error for unknown resource types", () => {
+      expect(() => {
+        can({ username: "user1" }, "add", "foo");
+      }).toThrow();
+    });
+  });
+});
