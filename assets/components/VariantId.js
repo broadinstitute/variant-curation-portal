022bdc435566f5f79a4e444fe3cd569058c00313
Riley Grant <grantrileyh@gmail.com>
2023-11-17 18:09:18 -0500

    Update variants page to include genome build

--- assets/components/VariantId.js
+++ assets/components/VariantId.js
@@ -2,18 +2,30 @@ import PropTypes from "prop-types";
 import React from "react";
 import { Popup } from "semantic-ui-react";
 
-const MAX_LENGTH = 25;
+const MAX_LENGTH = 20;
 
-const VariantId = ({ variantId }) => {
+const VariantId = ({ variantId, referenceGenome }) => {
+  const suffix = referenceGenome ? ` (${referenceGenome})` : "";
+
   if (variantId.length <= MAX_LENGTH) {
-    return <span>{variantId}</span>;
+    return <span>{`${variantId}${suffix}`}</span>;
   }
 
-  return <Popup trigger={<span>{variantId.slice(0, MAX_LENGTH)}...</span>} content={variantId} />;
+  return (
+    <Popup
+      trigger={<span>{`${variantId.slice(0, MAX_LENGTH)}...${suffix}`}</span>}
+      content={variantId}
+    />
+  );
 };
 
 VariantId.propTypes = {
   variantId: PropTypes.string.isRequired,
+  referenceGenome: PropTypes.string,
+};
+
+VariantId.defaultProps = {
+  referenceGenome: undefined,
 };
 
 export default VariantId;
